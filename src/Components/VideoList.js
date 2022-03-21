3bbb9eeca2c33bda7de37b7999aee2557c3165ac
mikedalu <allegrotech89@gmail.com>
2022-03-21 23:21:44 +0100

    prettier format

--- src/Components/VideoList.js
+++ src/Components/VideoList.js
@@ -1,21 +1,13 @@
+import React from "react";
+import VideoItem from "./VideoItem";
 
-import React from 'react';
-import VideoItem from './VideoItem'
+const VideoList = (props) => {
+	const videos = props.videos;
+	const renderedList = videos.map((video) => {
+		return <VideoItem key={video.id.videoId} video={video} onVideoSelect={props.onVideoSelect} />;
+	});
 
-
-const VideoList = (props)=>{
-
-    const videos = props.videos
-     const renderedList = videos.map((video)=>{
-         return <VideoItem key={video.id.videoId} video={video} onVideoSelect ={props.onVideoSelect}/>
-     })
-
-    return(
-        <div className="ui relaxed divided list">
-            {renderedList}
-        </div>
-        
-    )
-}
+	return <div className="ui relaxed divided list">{renderedList}</div>;
+};
 
 export default VideoList;
